cead9c496e32650fee88d2a6582e5b74f3e0e584
Alexandru Palade <alexpalade@gmail.com>
2020-04-12 12:40:37 +0300

    changed game object name

--- main.lisp
+++ main.lisp
@@ -3,12 +3,12 @@
 (defparameter *width* 800)
 (defparameter *height* 600)
 
-(gamekit:defgame example () ()
+(gamekit:defgame game () ()
   (:viewport-width *width*)
   (:viewport-height *height*)
   (:viewport-title "Society is Hard"))
 
-(defmethod gamekit:draw ((this example))
+(defmethod gamekit:draw ((this game))
   (gamekit:draw-text "Hello, world!" (gamekit:vec2 300 300)))
 
-(gamekit:start 'example)
+(gamekit:start 'game)
