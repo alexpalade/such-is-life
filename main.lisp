145a281437882521a0b9cf5503e53f8ca4d7f6c4
Alexandru Palade <alexpalade@gmail.com>
2020-04-12 18:05:26 +0300

    reverted travis config

--- main.lisp
+++ main.lisp
@@ -3,12 +3,12 @@
 (defparameter *width* 800)
 (defparameter *height* 600)
 
-(gamekit:defgame game () ()
+(gamekit:defgame sih () ()
   (:viewport-width *width*)
   (:viewport-height *height*)
   (:viewport-title "Society is Hard"))
 
-(defmethod gamekit:draw ((this game))
+(defmethod gamekit:draw ((this sih))
   (gamekit:draw-text "Hello, world!" (gamekit:vec2 300 300)))
 
-(gamekit:start 'game)
+(gamekit:start 'sih)
